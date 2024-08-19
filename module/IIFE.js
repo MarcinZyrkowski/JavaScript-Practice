9b02d5f29cc2fbcbc5585519e0c9b3072d7be9e1
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-08-19 15:24:24 +0200

    Typo

--- module/IIFE.js
+++ module/IIFE.js
@@ -1,4 +1,4 @@
-// immediately Invoked Function Expressions (IIFE) are JavaScript functions that are 
+// Immediately Invoked Function Expressions (IIFE) are JavaScript functions that are 
 // executed immediately after they are defined. They are typically used to create a 
 // local scope for variables to prevent them from polluting the global scope.
 
