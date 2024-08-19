f9a0a8cab2c9bfdf153a8ac17226ca6c85f9a087
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-08-19 14:24:02 +0200

    IIFE

---
+++ module/IIFE.js
@@ -0,0 +1,14 @@
+// immediately Invoked Function Expressions (IIFE) are JavaScript functions that are 
+// executed immediately after they are defined. They are typically used to create a 
+// local scope for variables to prevent them from polluting the global scope.
+
+// syntax
+// (function (){ 
+//      Function Logic Here. 
+//     })();
+
+(function() {
+	// IIFE code block
+	var localVar = 'This is a local variable';
+	console.log(localVar); // Output: This is a local variable
+})();
